fa9a4a2ef6977ad9fa92abeda2906b243bcfc28e
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2026-01-26 06:10:58 +0200

    Synchronization of projects

--- door_calculator/door_calculator/settings.py
+++ door_calculator/door_calculator/settings.py
@@ -31,7 +31,7 @@ M365_TECH_KEYWORD = os.getenv("M365_TECH_KEYWORD", "Чертежи в работ
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-fojb6-3@+u%rw5r2#)i5*3ra%4i-wd%2ne^*--(@vfa@b&-v0s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
